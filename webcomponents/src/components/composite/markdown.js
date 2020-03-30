731cf031da47a1ae4da699647230a2f8ea4313e4
TomasKulhanek <tmkulhanek@gmail.com>
2020-03-30 20:08:05 +0100

    UPDATE #70 replaced markdown component

--- webcomponents/src/components/composite/markdown.js
+++ webcomponents/src/components/composite/markdown.js
@@ -1,4 +1,6 @@
-import {Converter} from 'showdown';
+import Markdownit from 'markdown-it';
+import Markdownitfootnote from 'markdown-it-footnote';
+import hljs from 'highlightjs';
 import {I18N} from 'aurelia-i18n';
 import {bindable, inject} from 'aurelia-framework';
 import {HttpClient} from 'aurelia-fetch-client';
@@ -14,7 +16,24 @@ export class Markdown {
   }
 
   attached() {
-    this.converted = new Converter();
+    //console.log('makdownit attached hljs:', hljs);
+    // eslint-disable-next-line new-cap
+    this.md = Markdownit({
+      html: true, //enable html tags - this enables also custom elements of components/webcomponents
+      linkify: true,
+      typographer: true,
+      highlight: function(str, lang) { //highlight plugin
+        if (lang && hljs.getLanguage(lang)) {
+          try {
+            return '<pre class="hljs"><code>' +
+              hljs.highlight(lang, str, true).value +
+              '</code></pre>';
+          } catch (__) {}
+        }
+
+        return ''; // use external default escaping
+      }
+    }).use(Markdownitfootnote); //footnote - extension to MD - otherwise no link between [^1] and [^1]:
     //if (this.i18n.getLocale() === 'cs') { //czech version} else {//english version}
     //fetch md source from src attribute
     this.client.fetch(this.src)
@@ -22,7 +41,7 @@ export class Markdown {
       .then(data => {
         this.text = data;
         //convert from md to html
-        this.html = this.converted.makeHtml(this.text);
+        this.html = this.md.render(this.text);
       });
   }
 }
