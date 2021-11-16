0fbb43f325006ee79bed8e39cb900c486e20d044
Tomas Kulhanek <tomaton@centrum.cz>
2021-11-16 13:12:30 +0100

    UPDATE script

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
 # publish new version npm package 
 # - omit cleanup, otherwise testing on canvas will fail
 # need to install by `npm install canvas --build-from-sources` again
-np --no-cleanup
+np --no-cleanup --no-tests
